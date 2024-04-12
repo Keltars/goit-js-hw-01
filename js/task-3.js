f9961f110d59a767281871d80647b843376984e0
Keltars <keltars2000@gmail.com>
2024-04-12 08:50:55 +0300

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -8,3 +8,23 @@ const numericalBorder = Number.parseFloat(border);
 console.log(getElementWidth("50px", "8px", "4px"));
 console.log(getElementWidth("60px", "12px", "8.5px"));
 console.log(getElementWidth("200px", "0px", "0px"));
+
+console.log(Boolean('0'));
+
+console.log(3 > 2 > 1);
+
+console.log("5" + 5);
+
+console.log("10" > 5);
+
+console.log(Boolean(0));
+
+console.log(Number("0"));
+
+console.log("5" - 5);
+
+console.log("5" - true);
+
+console.log(true && 0 && 'їжачок');
+
+console.log();
